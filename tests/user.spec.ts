4cc950f3a001fb87e02862c1972e76d774921a47
jhiatt19 <hiattj19@gmail.com>
2026-02-21 23:45:54 -0700

    Added list users test

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -38,3 +38,15 @@ test("updateUser", async ({ page }) => {
 
   await expect(page.getByRole("main")).toContainText("pizza dinerx");
 });
+
+test("listUsers", async ({ page }) => {
+  await page.goto("/");
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByRole("textbox", { name: "Email address" }).fill("a@jwt.com");
+  await page.getByRole("textbox", { name: "Email address" }).press("Tab");
+  await page.getByRole("textbox", { name: "Password" }).fill("admin");
+
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.getByRole("link", { name: "Admin" }).click();
+  await page.getByRole("button", { name: "View Users" }).click();
+});
